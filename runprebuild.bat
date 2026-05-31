2763bd65f45747562c3fee93c5148b019933a4d7
Manfred Aabye <zainhofer@gmx.de>
2026-05-31 11:30:30 +0200

    Add files via upload

--- runprebuild.bat
+++ runprebuild.bat
@@ -2,31 +2,14 @@
 
 copy bin\System.Drawing.Common.dll.win bin\System.Drawing.Common.dll
 
-set "PREBUILD_EXE=Prebuild\src\bin\Release\prebuild.exe"
-set "MSBUILD_EXE="
+echo Building Prebuild generator...
+dotnet build Prebuild\src\Prebuild.csproj -c Release || goto :done
 
-if not exist "%PREBUILD_EXE%" (
-	echo Building Prebuild generator...
-	for /f "delims=" %%i in ('where msbuild 2^>NUL') do (
-		set "MSBUILD_EXE=%%i"
-		goto :build_prebuild
-	)
+copy /Y Prebuild\src\bin\Release\net10.0\prebuild.dll bin\prebuild.dll >NUL
+copy /Y Prebuild\src\bin\Release\net10.0\prebuild.runtimeconfig.json bin\prebuild.runtimeconfig.json >NUL
+copy /Y Prebuild\src\bin\Release\net10.0\prebuild.deps.json bin\prebuild.deps.json >NUL
 
-	if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" (
-		set "MSBUILD_EXE=%ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-		goto :build_prebuild
-	)
-
-	echo ERROR: MSBuild wurde nicht gefunden. Bitte Visual Studio 2022 Build Tools installieren.
-	goto :done
-)
-
-:build_prebuild
-if defined MSBUILD_EXE (
-	"%MSBUILD_EXE%" Prebuild\src\Prebuild.csproj /p:Configuration=Release /m || goto :done
-)
-
-"%PREBUILD_EXE%" /target vs2022 /targetframework net10_0 /excludedir = "obj | bin" /file prebuild.xml
+dotnet bin\prebuild.dll /target vs2022 /targetframework net10_0 /excludedir = "obj | bin" /file prebuild.xml
 if errorlevel 1 goto :done
 
     @echo Creating compile.bat
